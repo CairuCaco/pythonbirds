4d1dd07220dcb2fb511fabcd45312692a9bdbd7e
Cairu <cairutorresan@hotmail.com>
2022-02-07 15:11:38 -0300

    Criado tipo Homem

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,8 +7,22 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá{id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls}-olhos{cls.olhos}'
+
+
+class Homem(Pessoa):
+    pass
+
+
+
 if __name__ == '__main__':
-    renzo = Pessoa(nome='Renzo')
+    renzo = Homem(nome='Renzo')
     luciano = Pessoa(renzo, nome='Luciano')
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
@@ -29,3 +43,10 @@ if __name__ == '__main__':
     print(luciano.olhos)
     print(renzo.olhos)
     print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonima')
+    print(isinstance(pessoa ,Pessoa))
+    print(isinstance(pessoa ,Homem))
+    print(isinstance(renzo ,Pessoa))
+    print(isinstance(renzo ,Homem))
